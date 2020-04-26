2204444771da9084afe9bd25f36b15d185fd3fd7
Kovács Tamás <ombra@freemail.hu>
2020-04-26 20:36:37 +0200

    added all fire mode

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,8 +78,12 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        if (! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()) {
+          firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = true;
+        }
 
+        // if one of the stores are empty, the ship can't fire both torpedoes at once, return failure
         break;
     }
 
